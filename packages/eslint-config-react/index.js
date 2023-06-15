813b5beee57bbb5720bc1f006e73abf3b14302ef
192114 <shadowSun192114@gmail.com>
2023-06-15 18:06:46 +0800

    add vite template

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -5,13 +5,16 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
     'turbo',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
+  plugins: ['react-refresh'],
   rules: {
     'prettier/prettier': 'error',
     'arrow-body-style': 'off',
     'prefer-arrow-callback': 'off',
+    'react-refresh/only-export-components': 'warn',
   },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
